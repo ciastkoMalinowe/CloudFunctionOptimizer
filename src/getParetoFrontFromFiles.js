3609c04d92cb9b6ab576367cc95e2df80f58359b
Marcin Majewski <majewskimarcin@outlook.com>
2019-11-17 15:37:00 +0100

    * Adding count pareto front js file

---
+++ src/getParetoFrontFromFiles.js
@@ -0,0 +1,26 @@
+const pf = require('pareto-frontier');
+const fs = require('fs');
+
+const dirPath = process.argv[2];
+
+let files = fs.readdirSync(dirPath).filter(fn => fn.match('.*pareto.*'));
+
+
+let paretoFront = []
+
+for (let file of files) {
+    let array = fs.readFileSync(dirPath + "/" + file).toString().split("\n");
+    for (const elem of array) {
+        let sliced = elem.toString().split(',')
+        paretoFront.push([Number.parseFloat(sliced[0]), Number.parseFloat(sliced[1])])
+    }
+}
+
+console.log("Number of input points: " + paretoFront.length);
+
+paretoFront = pf.getParetoFrontier(paretoFront);
+let paretoFrontWriteStream = fs.createWriteStream('every_possible_output/paretoFrontAll.txt');
+paretoFront.forEach(function (point) {
+    paretoFrontWriteStream.write(point.join(', ') + '\n');
+});
+paretoFrontWriteStream.end();
