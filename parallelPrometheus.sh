119205851efd1c987bd07dc94087f1068b1e6914
Marcin Majewski <majewskimarcin@outlook.com>
2019-11-19 13:42:36 +0100

    * Adding bash script for prometheus

--- parallelPrometheus.sh
+++ parallelPrometheus.sh
@@ -10,5 +10,5 @@ do
   DAG=./results/step2/montage_AWS_256_512_1024x5/dag-every_possible$i.json
   echo "$DAG"
 	EXECUTION_STRING="app $TMP_TIMES $DAG $CONFIG $N_PROCESS $i"
-	sbatch --ntasks-per-node=1 -J "allPossible$i" --time=60 -N 1 --output="every_possible_output/output$i.log" "$EXECUTION_STRING"
+	sbatch --ntasks-per-node=1 -J "allPossible$i" --time=60 -N 1 --output="every_possible_output/output$i.log" batch $N_PROCESS $i
 done
