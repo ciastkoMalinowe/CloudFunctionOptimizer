dd77ae814902b6a81929dd566383f2aef7e78fdd
Marcin Majewski <majewskimarcin@outlook.com>
2019-11-19 18:14:38 +0100

    * Updating r script to generate plots

--- paretoFront.r
+++ paretoFront.r
@@ -1,21 +1,27 @@
-df <- read.csv("/home/mamajews/development/CloudFunctionOptimizer/every_possible_output/paretoFrontAll.txt")
-names(df) <- c('time','cost')
-
-single_point <- data.frame(35909,0.000415724)
-names(single_point) <- c('time','cost')
-
-sdbws <- data.frame(36962, 0.00040032)
-names(sdbws) <- c('time','cost') 
-
-require(ggplot2)
-library(ggrepel)
-plot <- ggplot(data = df, aes(x = time, y = cost)) 
-plot <- plot + geom_point(size = 1.5) 
-plot <- plot + geom_point(data = single_point, colour = "red", size = 1.5) 
-plot <- plot + geom_point(data = sdbws, colour = "green", size = 1.5, alpha =0.7)
-plot <- plot + geom_hline(yintercept=0.00045, linetype="dashed", color = "red")
-plot <- plot + geom_vline(xintercept=40000, linetype="dashed", color = "red")
-plot <- plot
-plot
-#+ geom_point(data = single_point, colour = "red", aes(x = time, y = cost)
-  
+    df <- read.csv("/home/mamajews/development/CloudFunctionOptimizer/every_possible_output/paretoFrontAll.txt")
+    names(df) <- c('time','cost')
+    
+    max_budget <- 0.0007034740000000003
+    min_budget <- 0.000382806
+    max_time <- 40224
+    min_time <- 18235
+    
+    
+    sdbcs <- data.frame(35909,0.000415724, 1.0, 1.0)
+    names(sdbcs) <- c('time','cost', 'time_constrain', 'cost_constrain')
+    
+    sdbws <- data.frame(36962, 0.00040032, 1.0, 1.0)
+    names(sdbws) <- c('time','cost', 'time_constrain', 'cost_constrain') 
+    
+    require(ggplot2)
+    library(ggrepel)
+    plot <- ggplot(data = df, aes(x = time, y = cost)) 
+    plot <- plot + geom_point(size = 1.5) 
+    plot <- plot + geom_point(data = sdbcs, colour = "red", size = 1.5) 
+    plot <- plot + geom_point(data = sdbws, colour = "green", size = 1.5, alpha =0.7)
+    plot <- plot + geom_hline(yintercept=min_budget, linetype="dashed", color = "red")
+    plot <- plot + geom_vline(xintercept=min_time, linetype="dashed", color = "red")
+    plot <- plot
+    plot
+    #+ geom_point(data = single_point, colour = "red", aes(x = time, y = cost)
+      
