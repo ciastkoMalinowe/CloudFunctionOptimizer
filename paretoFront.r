6b96eaca7999c5f9ca10030f4a7eab5b6514d75c
Marcin Majewski <majewskimarcin@outlook.com>
2019-11-19 18:47:12 +0100

    * Pareto front update

--- paretoFront.r
+++ paretoFront.r
@@ -5,8 +5,11 @@
     min_budget <- 0.000382806
     max_time <- 40224
     min_time <- 18235
-    
-    
+    deadline_parameter_time <- 0.5
+    deadline_parameter_cost <- 0.5
+    user_time_max <- min_time + deadline_parameter_time * (max_time - min_time)
+    user_cost_max <- min_budget + deadline_parameter_cost * (max_budget - min_budget)
+
     sdbcs <- data.frame(35909,0.000415724, 1.0, 1.0)
     names(sdbcs) <- c('time','cost', 'time_constrain', 'cost_constrain')
     
@@ -19,9 +22,7 @@
     plot <- plot + geom_point(size = 1.5) 
     plot <- plot + geom_point(data = sdbcs, colour = "red", size = 1.5) 
     plot <- plot + geom_point(data = sdbws, colour = "green", size = 1.5, alpha =0.7)
-    plot <- plot + geom_hline(yintercept=min_budget, linetype="dashed", color = "red")
-    plot <- plot + geom_vline(xintercept=min_time, linetype="dashed", color = "red")
-    plot <- plot
+    plot <- plot + geom_hline(yintercept=user_cost_max, linetype="dashed", color = "red")
+    plot <- plot + geom_vline(xintercept=user_time_max, linetype="dashed", color = "red")
+    plot <- plot 
     plot
-    #+ geom_point(data = single_point, colour = "red", aes(x = time, y = cost)
-      
