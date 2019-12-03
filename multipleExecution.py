e80b0e2023b983065877cad692ad7b88a9de36f9
Marcin Majewski <majewskimarcin@outlook.com>
2019-12-03 15:04:12 +0100

    * filePath to file_path

--- multipleExecution.py
+++ multipleExecution.py
@@ -5,16 +5,16 @@ import subprocess
 
 parser = argparse.ArgumentParser(description="Modifying config.json file, executing algorithms, with every possible "
                                              "combinations (budget x cost)")
-parser.add_argument('-fp', '--filePath', action="store", dest="filePath", help="path to config file", default="")
+parser.add_argument('-fp', '--filePath', action="store", dest="file_path", help="path to config file", default="")
 parser.add_argument('-bp', '--budgetParameters', nargs="+", action="store", dest="budgetParameters")
 parser.add_argument('-dp', '--deadlineParameters', nargs="+", action="store", dest="deadlineParameters")
 parser.add_argument('-alg', '--algorithms', nargs="+", action="store", dest="algorithms")
 
 
-def change_json_key(jsonKey, newValue, filePath):
-    with open(filePath, 'r+') as f:
+def change_json_key(json_key, new_value, file_path):
+    with open(file_path, 'r+') as f:
         data = json.load(f)
-        data[jsonKey] = newValue
+        data[json_key] = new_value
         f.seek(0)
         json.dump(data, f, indent=4)
         f.truncate()
@@ -31,11 +31,11 @@ args = parser.parse_args()
 for algorithm in args.algorithms:
     for budgetParameter in args.budgetParameters:
         for deadline in args.deadlineParameters:
-            change_json_key("budgetParameter", budgetParameter, args.filePath)
-            change_json_key("deadlineParameter", deadline, args.filePath)
-            change_json_key("algorithm", algorithm, args.filePath)
+            change_json_key("budgetParameter", budgetParameter, args.file_path)
+            change_json_key("deadlineParameter", deadline, args.file_path)
+            change_json_key("algorithm", algorithm, args.file_path)
             delete_results_folder()
-            process = subprocess.Popen(["./scripts/step2.sh", args.filePath], stdout=subprocess.PIPE)
+            process = subprocess.Popen(["./scripts/step2.sh", args.file_path], stdout=subprocess.PIPE)
             for line in iter(process.stdout.readline, b''):
                 print(str(line))
             process.stdout.close()
