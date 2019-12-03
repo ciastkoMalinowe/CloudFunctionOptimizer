c2b1ca93b58636c7329870b314467b69d380e7ec
Marcin Majewski <majewskimarcin@outlook.com>
2019-12-03 01:20:36 +0100

    * Plot created

--- paretoFront.r
+++ paretoFront.r
@@ -1,58 +1,63 @@
-    df <- read.csv("/home/mamajews/Development/CloudFunctionOptimizer/every-possible-wf_0.02-256x512x1024-1_1/paretoFrontAll.txt")
-    names(df) <- c('time','cost')
-    type = 'pareto'
-    df <- cbind(df, type)
-    
-    
-    moheft <- read.csv('/home/mamajews/Development/CloudFunctionOptimizer/moheft.csv')
-    names(moheft) <- c('time', 'cost')
-    type = 'moheft-pareto'
-    moheft <- cbind(moheft, type)
-    
-    df <- rbind(df, moheft)
-    
-    max_budget <- 0.0007034740000000003
-    min_budget <- 0.000382806
-    max_time <- 40224
-    min_time <- 18235
-    deadline_parameter_time <- 0.7
-    deadline_parameter_cost <- 0.7
-    user_time_max <- min_time + deadline_parameter_time * (max_time - min_time)
-    user_cost_max <- min_budget + deadline_parameter_cost * (max_budget - min_budget)
+        df <- read.csv("/home/mamajews/Development/CloudFunctionOptimizer/outputs_multiple/all_montage-0.25.txt")
+        names(df) <- c('time','cost','type','deadlineParameter','budgetParameter', 'deadline', 'budget')
+        
+        
+        require(tidyverse)
+        require(ggplot2)
+        require(gridExtra)
+        require(ggpubr)
+        df$cost <- as.numeric(as.character(df$cost))
+        curr_df <- df
+        
+        max_budget <- 0.00313396
+        min_budget <- 0.000709930000000001
+        max_time <- 59966
+        min_time <- 14727
 
-    #sdbcs <- data.frame(35909,0.000415724, 1.0, 1.0)
-    #sdbcs <- data.frame(35909,0.000415724, 0.9, 0.9)
-    sdbcs <- data.frame(35909,0.000406575, 0.7, 0.7)
-    
-    names(sdbcs) <- c('time','cost', 'time_constrain', 'cost_constrain')
-    
-    #sdbws <- data.frame(36962, 0.00040032, 1.0, 1.0)
-    #sdbws <- data.frame(36962, 0.00040032, 0.9, 0.9)
-    sdbws <- data.frame(36962, 0.00040032, 0.7, 0.7)
-    
-    names(sdbws) <- c('time','cost', 'time_constrain', 'cost_constrain') 
-    
-    require(ggplot2)
-    plot <- ggplot(data = df, aes(x = time, y = cost, col=df$type))
-    
-    plot <- plot + coord_cartesian(xlim = c(min(df[,1]), max(df[,1])), ylim = c(min(df[,2]), max(df[,2])))
-    
-    plot <- plot + scale_color_manual(name = "",breaks = c ("pareto", "sdbcs", "sdbws", "accepted solutions", "moheft-pareto"), values = c("black", "red", "green", "blue", "rose"))
-    plot <- plot + geom_point(size = 1.5) 
-    plot <- plot + geom_point(data = sdbcs, aes(color = "sdbcs", x = sdbcs$time, y=sdbcs$cost), size = 1.5) 
-    plot <- plot + geom_point(data = sdbcs, aes(color = "sdbws", x = sdbws$time, y=sdbws$cost), size = 1.5) 
-    #plot <- plot + geom_point(data = moheft, aes(color = "moheft", x = sdbcs$time, y=sdbcs$cost), size = 1.5) 
-    
-    #plot <- plot + geom_hline(yintercept=user_cost_max, linetype="dashed", color = "blue")
-    #plot <- plot + geom_vline(xintercept=user_time_max, linetype="dashed", color = "blue")
-    plot <- plot + theme(legend.position = "bottom")
-    
-    plot <- plot + geom_rect(data = df[1,], aes(xmin = min(df[,1] - 10000), xmax = user_time_max, ymax = user_cost_max, ymin=min(df[,2])- 0.5), fill="blue", inherit.aes = FALSE, alpha = 0.1)
-    
-    #plot <- plot + annotate(geom="text", label = "1.0", x = user_time_max + 2000, y = user_cost_max + 0.000005, col = 'blue')
-    #plot <- plot + annotate(geom="text", label = "1.0", x = user_time_max + 2000, y = user_cost_max + 0.000005, col = 'blue')
-    
-    
-    
-    
-    plot
+        curr_df <- df %>% filter(budgetParameter == 0.2)
+        user_time_max <- curr_df[1,]$deadline
+        user_cost_max <- curr_df[1,]$budget
+        plot <- ggplot(data = curr_df, aes(x = time, y = cost, col=curr_df$type)) + xlim(min_time, max_time) + ylim(min_budget, max_budget)
+        #plot <- plot + scale_color_manual(name = "",breaks = c ("moheft", "sdbcs", "moheft-loss", "sdbws", "pareto"), values = c("black", "red", "green", "blue", "rose"))
+        plot <- plot + geom_point(size = 1.5) 
+        plot <- plot + theme(legend.position = "bottom")
+        plot <- plot + ggtitle('b = 0.2, c = 0.2')
+        plot <- plot + geom_hline(yintercept = user_cost_max)
+        plot <- plot + geom_vline(xintercept  = user_time_max)
+
+        curr_df1 <- df %>% filter(budgetParameter == 0.1)
+        user_time_max <- curr_df1[1,]$deadline
+        user_cost_max <- curr_df1[1,]$budget
+        plot1 <- ggplot(data = curr_df, aes(x = time, y = cost, col=curr_df1$type)) + xlim(min_time, max_time) + ylim(min_budget, max_budget)
+       # plot1 <- plot1 + scale_color_manual(name = "",breaks = c ("moheft", "sdbcs", "moheft-loss", "sdbws", "pareto"), values = c("black", "red", "green", "blue", "rose"))
+        plot1 <- plot1 + geom_point(size = 1.5)
+        plot1 <- plot1 + theme(legend.position = "bottom")
+        plot1 <- plot1 + ggtitle('b = 0.1, c = 0.1')
+        plot1 <- plot1 + geom_hline(yintercept = user_cost_max)
+        plot1 <- plot1 + geom_vline(xintercept  = user_time_max)
+
+        curr_df2 <- df %>% filter(budgetParameter == 0.4)
+        user_time_max <- curr_df2[1,]$deadline
+        user_cost_max <- curr_df2[1,]$budget
+        plot2 <- ggplot(data = curr_df, aes(x = time, y = cost, col=curr_df2$type)) + xlim(min_time, max_time) + ylim(min_budget, max_budget)
+        #plot2 <- plot2 + scale_color_manual(name = "",breaks = c ("moheft", "sdbcs", "moheft-loss", "sdbws", "pareto"), values = c("black", "red", "green", "blue", "rose"))
+        plot2 <- plot2 + geom_point(size = 1.5)
+        plot2 <- plot2 + theme(legend.position = "bottom")
+        plot2 <- plot2 + ggtitle('b = 0.4, c = 0.4')
+        plot2 <- plot2 + geom_hline(yintercept = user_cost_max)
+        plot2 <- plot2 + geom_vline(xintercept  = user_time_max)
+
+        curr_df3 <- df %>% filter(budgetParameter == 0.6)
+        user_time_max <- curr_df3[1,]$deadline
+        user_cost_max <- curr_df3[1,]$budget
+        plot3 <- ggplot(data = curr_df3, aes(x = time, y = cost, col=curr_df3$type)) + xlim(min_time, max_time) + ylim(min_budget, max_budget)
+        #plot2 <- plot2 + scale_color_manual(name = "",breaks = c ("moheft", "sdbcs", "moheft-loss", "sdbws", "pareto"), values = c("black", "red", "green", "blue", "rose"))
+        plot3 <- plot3 + geom_point(size = 1.5)
+        plot3 <- plot3 + theme(legend.position = "bottom")
+        plot3 <- plot3 + ggtitle('b = 0.4, c = 0.4')
+        plot3 <- plot3 + geom_hline(yintercept = user_cost_max)
+        plot3 <- plot3 + geom_vline(xintercept  = user_time_max)
+
+        
+        ggarrange(plot1,plot,plot2,plot3, common.legend = TRUE)
+        
