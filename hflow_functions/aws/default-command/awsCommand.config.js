9309394e4b196b279cda597e7ddf95076a1ebfb9
Marcin Majewski <majewskimarcin@outlook.com>
2019-12-15 23:49:37 +0100

    * Command config updated

--- hflow_functions/aws/default-command/awsCommand.config.js
+++ hflow_functions/aws/default-command/awsCommand.config.js
@@ -1,12 +1,19 @@
 const FUNCTION_TYPE = process.env.FUNCTION_TYPE ? process.env.FUNCTION_TYPE : "1536";
 
-const AWS_BUCKET = "hyperflow.workflow.test";
-const AWS_PATH = "results";
+const AWS_BUCKET = "hyperflow.ellipsoids";
+const AWS_PATH = "run";
 
 exports.functionType = FUNCTION_TYPE;
 
 exports.resources = {
-  "256": "https://tm8yi4eva1.execute-api.eu-central-1.amazonaws.com/dev/aws-executor-256"
+  "128": "aws-executor-ellipsoids-dev-aws-executor-128",
+  "256": "aws-executor-ellipsoids-dev-aws-executor-256",
+  "512": "aws-executor-ellipsoids-dev-aws-executor-512",
+  "1024": "aws-executor-ellipsoids-dev-aws-executor-1024",
+  "1536": "aws-executor-ellipsoids-dev-aws-executor-1536",
+  "2048": "aws-executor-ellipsoids-dev-aws-executor-2048",
+  "2560": "aws-executor-ellipsoids-dev-aws-executor-2560",
+  "3008": "aws-executor-ellipsoids-dev-aws-executor-3008",
 };
 
 // Google cloud storage
