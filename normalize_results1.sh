d22f8c0e17d50f71b5df11c2bd61afda8c59d83c
Marcin Majewski <majewskimarcin@outlook.com>
2020-02-23 18:49:04 +0100

    * Finishing all_vina

---
+++ normalize_results1.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+scriptdir=`dirname "$0"`
+appdir=`dirname "${scriptdir}"`
+configPath=$1
+config=${appdir}/${configPath}
+normalizer=${appdir}/dagscripts/normalizer.js
+dagPath=`jq -r '.dag' ${config}`
+provider=`jq -r '.provider' ${config}`
+count=`jq '.count' ${config}`
+workflow=`jq -r '.workflow' ${config}`
+functionTypesTitle=`jq -r '.functionTypes | join("_")' ${config}`
+
+outputFolder=${appdir}/results/step1/${workflow}_${provider}_${functionTypesTitle}x${count}
+outputFile=${outputFolder}/normalized_logs.csv
+
+# Step 1
+# Script responsible for running passed
+# function types on lambdas a given number of times
+# It returns normalized output all of functions
+# Run with ./scripts/step1.sh  <path_to_configuration>
+
+# Check if results already exists
+#if [[ -d "$outputFolder" ]] ;then
+#    echo Results ${workflow}_${provider}_${functionTypesTitle}x${count} already exists in path: ${outputFolder}
+#    echo Delete folder \"${outputFolder}\" to have a new data and try again
+#    exit 0
+#fi
+
+mkdir -p "${outputFolder}"
+
+if [[ ! -d "$outputFile" ]] ;then
+  printf "task id resource request_start request_end request_duration start end time downloaded executed uploaded type\n" > "${outputFile}"
+else
+  echo "Summary file already exist, new results will be appended"
+fi
+
+for functionType in $(jq -r '.functionTypes[]' ${config}); do
+    echo Executing workflow for type: ${functionType}
+    export FUNCTION_TYPE=${functionType};
+    folder=${appdir}/results/step1/${workflow}_${provider}_${functionType}x${count}
+
+    if [[ ! -d "$folder" ]] ;then
+        mkdir -p ${folder}
+    else
+        echo "Already exists: ${folder}. Not procedding for function type: ${functionType}"
+        continue
+    fi
+
+    echo Saving to ${folder}
+    for ((i = 1; i <= count; i++))
+    do
+        if [[ ! -d "$folder/logs_$i.txt" ]] ;then
+            # Run Hyperflow
+            if [[ "$workflow" == "ellipsoids" ]] ;then
+                # !!! Temporary HACK for running ellipsoids workflow !!!
+                # Because hyperflow for some reason doesn't reach post-processing tasks during ellipsoids
+                # workflow execution, the program must be terminated manually
+                expect -c "set timeout 1000; spawn ${appdir}/node_modules/hyperflow/bin/hflow run ${dagPath} -s; expect \", executable: summary.js\" {close}" >> ${folder}/logs_${i}.txt
+            else
+                ${appdir}/node_modules/hyperflow/bin/hflow run ${dagPath} -s >> ${folder}/logs_${i}.txt
+            fi
+
+            echo Workflow run ${i} finished! Parsing response...
+            ${appdir}/scripts/parse_log.sh ${folder}/logs_${i}.txt ${functionType} ${provider} >> ${folder}/logs_${i}.json
+
+            # Normalize
+            node ${normalizer} ${folder}/logs_${i}.json ${outputFile}
+        fi
+        sleep 15
+    done
+done
