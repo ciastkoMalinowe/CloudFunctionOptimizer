3ac71fcae3d5c3c3457945c2bf2c5e0e673b9165
Marcin Majewski <majewskimarcin@outlook.com>
2020-04-19 17:09:35 +0200

    * R script for real execution added

--- rScripts/execution.r
+++ rScripts/execution.r
@@ -2,7 +2,7 @@ library(tidyverse)
 require(ggplot2)
 library(tikzDevice)
 
-df <- read.csv('/home/mamajews/Development/CloudFunctionOptimizer/results/step1/vina_AWS_128_256_512_1024_1536_2048_2560_3008x5/normalized_logs.csv', 
+df <- read.csv('/home/mamajews/Development/normalized_logs.csv',
                stringsAsFactors = FALSE,
                sep="")
 df <- df %>% filter(resource == 128)
@@ -11,7 +11,7 @@ minimum <- min(df$request_start)
 df$request_start <- (df$request_start - minimum) / 1000 
 df$request_end <- (df$request_end - minimum) / 1000
 int_breaks <- function(x, n = 5) pretty(x, n)[pretty(x, n) %% 1 == 0] 
-tikz('plot1.tex',width=3.5, height=3)
+tikz('/home/mamajews/Development/Lambda-Scheduling-MSc/images/ellipsoids-small-execution.tex',width=3.5, height=3)
 ggplot(df) +
   geom_segment( aes(x=df$id, xend=df$id, y=df$request_start, yend=df$request_end), color="grey") +
   geom_point( aes(x=df$id, y=df$request_start), color=rgb(0.2,0.7,0.1,0.5), size=1 ) +
---
+++ rScripts/executionRealPlanned.r
@@ -0,0 +1,38 @@
+library(tidyverse)
+require(ggplot2)
+library(tikzDevice)
+library(jsonlite)
+
+
+scheduled <- read.csv('/home/mamajews/Development/CloudFunctionOptimizer/results/step2/montage_AWS_512_1024_1536_2048_2560_3008x5/scheduled.csv', sep = "\t", header = FALSE)
+names(scheduled) <- c('id', 'functionType', 'startTime', 'endTime')
+scheduled$startTime <- (scheduled$startTime) / 1000
+scheduled$endTime <- (scheduled$endTime ) / 1000
+scheduled$functionType = as.character(scheduled$functionType)
+
+ggplot(scheduled) +
+  geom_segment(aes(x = id, xend = id, y = startTime, yend = endTime, colour=functionType), size=5) +
+  # geom_rect(aes(x = id, xend = id, y = startTime, yend = endTime), color = "grey", ) +
+  # geom_point( aes(x=id, y=startTime), color=rgb(0.2,0.7,0.1,0.5), size=2 ) +
+  # geom_point( aes(x=id, y=endTime), color=rgb(0.7,0.2,0.1,0.5), size=2) +
+  coord_flip()+
+  xlab("Task id") +
+  ylab("Time [s]")
+
+  #
+  # minimum <- min(df$request_start)
+  # df$request_start <- (df$request_start - minimum) / 1000
+  # df$request_end <- (df$request_end - minimum) / 1000
+  # int_breaks <- function(x, n = 5) pretty(x, n)[pretty(x, n) %% 1 == 0]
+  # tikz('/home/mamajews/Development/Lambda-Scheduling-MSc/images/ellipsoids-small-execution.tex',width=3.5, height=3)
+  # ggplot(df) +
+  #   geom_segment( aes(x=df$id, xend=df$id, y=df$request_start, yend=df$request_end), color="grey") +
+  #   geom_point( aes(x=df$id, y=df$request_start), color=rgb(0.2,0.7,0.1,0.5), size=1 ) +
+  #   geom_point( aes(x=df$id, y=df$request_end), color=rgb(0.7,0.2,0.1,0.5), size=1 ) +
+  #   coord_flip()+
+  #   theme(
+  #     legend.position = "none",
+  #   ) +
+  #   xlab("Task id") +
+  #   ylab("Time [s]")
+   # dev.off()
